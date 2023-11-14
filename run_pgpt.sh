67f4d01ba0e896f2c951fb55d1cdc571f8b6bb3f
Henrique Pinheiro <henrique@gampoc.com>
2023-11-14 12:16:56 -0300

    Add poetry config for max-workers

--- run_pgpt.sh
+++ run_pgpt.sh
@@ -4,6 +4,7 @@ if command -v nvidia-smi > /dev/null
 then
   CMAKE_ARGS='-DLLAMA_CUBLAS=on' poetry run pip install llama-cpp-python
 fi
+poetry config installer.max-workers 10
 poetry install --with ui,local
 poetry run python scripts/setup
 PGPT_PROFILES=local make run
